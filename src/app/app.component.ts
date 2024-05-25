05e86a51af985ada42053875330ed8a4079fd26e
Darril <53953682+Darril44K@users.noreply.github.com>
2024-05-25 20:46:35 +0200

    creation du header avec son style + interface cocktails

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Cocktail } from './interfaces/cocktail.interface';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cocktails';
+
+  cocktail : Cocktail = {
+    name: 'Mojito',
+    img : 'assets/mojito.jpg',
+    description: "test"
+  }
 }
